c5fea1884e26e1d87f1cf756ade1daab5ccc6624
joeyo991 <joeyosull@gmail.com>
2022-01-25 16:38:43 +0000

    Create simple app to calculate tips in work on sundays

--- run.py
+++ run.py
@@ -1,25 +1,28 @@
-def calculate_total_hours_worked():
+def main():
+    """
+    Runs main function
+    """
     joey_hours = int(input("How many hours did Joey work? "))
     hannah_hours = int(input("How many hours did Hannah work? "))
     rachel_hours = int(input("How many hours did Rachel work? "))
+    willian_hours = int(input("How many hours did Willian work? "))
     ciara_hours = int(input("How many hours did Ciara work? "))
-    willy_hours = int(input("How many hours did Willy work? "))
     saoirse_hours = int(input("How many hours did Saoirse work? "))
-    total_hours_worked = (joey_hours + hannah_hours + rachel_hours + ciara_hours + willy_hours + saoirse_hours)
-    
-    print(f"The total hours worked: {total_hours_worked}")
-    return total_hours_worked
-
-
-def calculate_tips_per_hour():
-    total_tips = int(input("What were the total tips? "))
-    tips_per_hour = total_tips / total_hours_worked
-
-    print(tips_per_hour)
-
-def main():
-    calculate_total_hours_worked()
-    calculate_tips_per_hour()
-
+    total_hours = (
+        joey_hours + hannah_hours + rachel_hours +
+        willian_hours + ciara_hours + saoirse_hours
+    )
+    print(f"The total amount of hours worked was {total_hours}")
+    total_tips = int(input("What were the total tips today? "))
+    tips_per_hour = total_tips / total_hours
+    rounded_tips_per_hour = round(tips_per_hour, 2)
+    print(f"The tips per hour is {rounded_tips_per_hour}")
+    print("Calculationg everyones tips...")
+    print(f"Joey's tips today were {joey_hours * rounded_tips_per_hour}")
+    print(f"Hannah's tips today were {hannah_hours * rounded_tips_per_hour}")
+    print(f"Rachel's tips today were {rachel_hours * rounded_tips_per_hour}")
+    print(f"Willian's tips today were {willian_hours * rounded_tips_per_hour}")
+    print(f"Ciara's tips today were {ciara_hours * rounded_tips_per_hour}")
+    print(f"Saoirse's tips today were {saoirse_hours * rounded_tips_per_hour}")
 
 main()
